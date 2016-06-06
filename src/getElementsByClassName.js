5d6295160385ace616ff0dc17c6515acdc050523
Heather Park <hthr.prk@gmail.com>
2016-06-06 15:58:40 -0700

    Rewrite getElementsByClassName() and stringifyJSON() functions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className, node){
+
+  node = node || document.body;
+  //container for all matching elements
+    var results = [];
+
+  //find if the element has the classname
+    if (node.classList && node.classList.contains(className) > 0 ) {
+      //if it matches push the result to the array
+      results.push(node);
+    }
+    //look to see if node has childNodes
+    if (node.childNodes.length > 0) {
+      //loop through childNodes and put through getElements Function
+      for (var i = 0; i < node.childNodes.length; i++) {
+        //invoke recursion function on the current childNode
+        var childResults = getElementsByClassName(className, node.childNodes[i]);
+        results = results.concat(childResults);
+      }
+    }
+    return results;
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  // if obj is an array
+  if (Array.isArray(obj)) {
+    var results = [];
+
+    for (var i = 0; i < obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
+    }
+
+    results = results.join(',');
+
+    return '[' + results + ']';
+  }
+
+  // if obj is an object
+  if (typeof(obj) === 'object') {
+    if (obj !== null) {
+      var results = [];
+
+      for (var key in obj) {
+        if (typeof obj[key] === 'function' || obj[key] === undefined) {
+          continue;
+        } else {
+          results.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+        }
+      }
+
+      results = results.join(',');
+
+      return "{" + results + "}";
+    }
+  }
+
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+
+  // if obj is number, boolean, null
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return '' + obj;
+  }
+
 };
